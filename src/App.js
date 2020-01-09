9614551b677c2d89d8710acbf18aaf55e88b274c
Andrew Vagliano <andrewvagliano2021@u.northwestern.edu>
2020-01-09 16:39:05 -0500

    Slice 0: Working App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD3HB4Nj8bbp5JaTxM0VyUee3o7L6u9oec",
+    authDomain: "new-shopping-cart-998c8.firebaseapp.com",
+    databaseURL: "https://new-shopping-cart-998c8.firebaseio.com",
+    projectId: "new-shopping-cart-998c8",
+    storageBucket: "new-shopping-cart-998c8.appspot.com",
+    messagingSenderId: "926357769629",
+    appId: "1:926357769629:web:42055e15ab7a4513497374"
+};  
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
 
 function App() {
   return (
